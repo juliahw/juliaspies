b6a1ebffaf92eb224ab2e4947bfd4d2d2b32030d
juliahw <juliahw@princeton.edu>
2014-09-10 22:59:31 -0400

    Wrote homepage

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-mygithubpage
-============
+
+#Julia's Pies
+
+#####Nothing tastes more like home than a slice of apple pie.
+
+This is the website for Julia's Pies, my pie catering service on Princeton campus.
---
+++ js/quotes.js
@@ -0,0 +1,22 @@
+var quotes = [
+    {
+        quote: "But I, when I undress me Each night upon my knees Will ask the Lord to bless me with apple pie...",
+        author: "Eugene Field, 1850-1895",
+        url: "img/field.png"
+    },
+    {
+        quote: "Men may come and men may go...but Pie goes on forever.",
+        author: "George Augustus Sala, 1828-1895",
+        url: "img/sala.png"
+    },
+    {
+        quote: "Good apple pies are a considerable part of our domestic happiness.",
+        author: "Jane Austen, 1775-1817",
+        url: "img/austen.png"
+    },
+    {
+        quote: "I don't want to spend my life not having good food going into my pie hole. That hole was made for pies.",
+        author: "Paula Deen, 1947-present",
+        url: "img/deen.png"
+    }
+];
