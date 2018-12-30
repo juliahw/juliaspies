7bae98ed75670a681dfeb63695ed14673ac57fca
Julia Wang <julia.wang@airbnb.com>
2018-12-30 00:48:56 -0800

    Close shop

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,54 +11,8 @@ const Home = () => (
       <div className="span8">
         <h1>Julia’s Pies</h1>
         <p>
-          <span className="text-red">Julia’s Pies</span> is a pie shop serving fresh baked goods from Julia’s kitchen, with the hope that every bite will bring you happy times.
+          We regret to inform you that Julia’s Pies is on <span className="text-red">indefinite hiatus</span>. It’s been a good run, and we’re so excited to begin a new chapter. Thanks everybody for your love and support. Don’t be a stranger. :)
         </p>
-      </div>
-    </div>
-
-    <div className="row">
-      <div className="span12">
-        <Link to="/order">
-          <div className="btn space2">
-              <h3 className="text-white">Order Now</h3>
-          </div>
-        </Link>
-      </div>
-    </div>
-
-    <div className="row">
-      <div className="span8">
-        <h2>Menu</h2>
-        <p>
-          Thanksgiving might be over, but the holiday season has just begun. Bring home one of our late fall menu items today to share with friends & family!
-        </p>
-      </div>
-    </div>
-
-    <div className="row">
-      <div className="span6">
-        {
-          menuItems.slice(0, 2).map((item) => (
-            <MenuItem
-              key={item.id}
-              name={item.name}
-              description={item.description}
-              prices={[{price: item.price}]}
-            />
-          ))
-        }
-      </div>
-      <div className="span6">
-        {
-          menuItems.slice(2).map((item) => (
-            <MenuItem
-              key={item.id}
-              name={item.name}
-              description={item.description}
-              prices={[{price: item.price}]}
-            />
-          ))
-        }
       </div>
     </div>
 
