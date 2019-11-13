54d5c986f1fc4eff0e4c547d14136d1f0ff2aacd
Julia Wang <juliawang240@gmail.com>
2019-11-13 10:55:56 -0800

    Set up the Thanksgiving pop-up

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { Route, Link, Switch } from "react-router-dom";
 
-import Home from "./Home";
-import Form from "./Form";
+import Popup from "./Popup";
+import PopupForm from "./PopupForm";
 
 import Logo from "../images/logo.png";
 
@@ -13,17 +13,9 @@ class App extends React.Component {
   render() {
     return (
       <div className="container">
-        <div className="row">
-          <div className="span12">
-            <Link to="/">
-              <img id="logo" src={Logo} alt="Julia's Pies" />
-            </Link>
-          </div>
-        </div>
-
         <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/order" component={Form} />
+          <Route exact path="/" component={Popup} />
+          <Route path="/order" component={PopupForm} />
         </Switch>
       </div>
     );
