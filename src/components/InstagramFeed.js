1230d06b600a0c942917a484026d87349a083f39
Julia Wang <juliawang240@gmail.com>
2020-10-03 15:10:11 -0700

    Refresh the Instagram token

--- src/components/InstagramFeed.js
+++ src/components/InstagramFeed.js
@@ -5,7 +5,7 @@ class InstagramFeed extends React.Component {
   componentDidMount() {
     const instafeed = new Instafeed({
       accessToken:
-        "IGQVJWekVMTjlVREI3aWJ0M1ZA3VklaWDRBMnI5SE1rSGNOVHdNVEVwT1hBWDROd3ZARZAXFDc1dQczVtbXpXQ3FsN1pOR0lFRVIzQUNLSFU0NTg1RmNJYy00RjFDaUxyNEh6c2hZATWtRWGNkcEVrWlRZAawZDZD",
+        "IGQVJYQk4xcWo1UGlMQVNGMXJtY21jSkN4X1h3OUVEYzNDTzBONVFmS3Vqa2FySFNDdlpXcW9rM09EWnlsX1h0djlCT2hxY05yYkFSNnNRTkU4Rlh6NTVIZADFRUktINTIteld3a1FoSkZAZAdVZABNkRraQZDZD",
       template:
         '<div class="span4"><a href="{{link}}" target="_blank" id="{{id}}"><img src="{{image}}" /></a></div>',
       limit: 3
