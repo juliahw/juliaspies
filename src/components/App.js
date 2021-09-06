fb2b02ddeb4ecd0be21820d4d91cec5f6101e138
Julia Wang <juliawang240@gmail.com>
2021-09-06 00:32:18 -0700

    Delete some cruft

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,13 @@
 import React from "react";
 import { Route, Link, Switch } from "react-router-dom";
-
 import Home from "./Home";
-
-import Logo from "../images/logo.png";
-
-const FACEBOOK_LINK = "https://www.facebook.com/juliamakespies";
-const INSTAGRAM_LINK = "https://www.instagram.com/juliamakespies";
 
 class App extends React.Component {
   render() {
     return (
-      <div className="container">
-        <Switch>
-          <Route exact path="/" component={Home} />
-        </Switch>
-      </div>
+      <Switch>
+        <Route exact path="/" component={Home} />
+      </Switch>
     );
   }
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,61 +1,60 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
-import menuItems from "../menuItems";
-// import Follow from "./Follow";
 import InstagramFeed from "./InstagramFeed";
 
 const Home = () => (
-  <div className="content">
-    <header>
-      <h1>Julia’s Pies</h1>
-    </header>
+  <div className="container">
+    <div className="content">
+      <header>
+        <h1>Julia’s Pies</h1>
+      </header>
 
-    <main>
-      <div className="row">
-        <div className="span8">
-          <p>
-            Julia’s Pies is the product of my lifelong obsession with pie. I
-            develop my own recipes to share with family and friends inspired by
-            food science, seasonality, and flavors. Sometimes I hold giveaways
-            and seasonal sales. In the past I have catered pie for{" "}
-            <a href="https://www.seedandsoulclub.org/" target="_blank">
-              Seed and Soul Club
-            </a>{" "}
-            and{" "}
-            <a
-              href="https://www.facebook.com/groups/sfbayareahoya"
-              target="_blank"
-            >
-              Bay Area Hoya
-            </a>{" "}
-            events.
-          </p>
-          <p>Interested in pie? Techniques? Recipes? Reach out anytime!</p>
+      <main>
+        <div className="row">
+          <div className="span8">
+            <p>
+              Julia’s Pies is the product of my lifelong obsession with pie. I
+              develop my own recipes to share with family and friends inspired
+              by food science, seasonality, and flavors. Sometimes I hold
+              giveaways and seasonal sales. In the past I have catered pie for{" "}
+              <a href="https://www.seedandsoulclub.org/" target="_blank">
+                Seed and Soul Club
+              </a>{" "}
+              and{" "}
+              <a
+                href="https://www.facebook.com/groups/sfbayareahoya"
+                target="_blank"
+              >
+                Bay Area Hoya
+              </a>{" "}
+              events.
+            </p>
+            <p>Interested in pie? Techniques? Recipes? Reach out anytime!</p>
+          </div>
         </div>
-      </div>
 
-      <InstagramFeed />
-    </main>
+        <InstagramFeed />
+      </main>
 
-    <footer>
-      <div className="footer-item">
-        <div className="heading-sm">Instagram</div>
-        <div>
-          <a href="https://www.instagram.com/juliamakespies" target="_blank">
-            @juliamakespies
-          </a>
+      <footer>
+        <div className="footer-item">
+          <div className="heading-sm">Instagram</div>
+          <div>
+            <a href="https://www.instagram.com/juliamakespies" target="_blank">
+              @juliamakespies
+            </a>
+          </div>
         </div>
-      </div>
-      <div className="footer-item">
-        <div className="heading-sm">Email</div>
-        <div>juliawang240@gmail.com</div>
-      </div>
-      <div className="footer-item">
-        <div className="heading-sm">Location</div>
-        <div>San Francisco, CA</div>
-      </div>
-    </footer>
+        <div className="footer-item">
+          <div className="heading-sm">Email</div>
+          <div>juliawang240@gmail.com</div>
+        </div>
+        <div className="footer-item">
+          <div className="heading-sm">Location</div>
+          <div>San Francisco, CA</div>
+        </div>
+      </footer>
+    </div>
   </div>
 );
 
